9071d3940e586969d637665a31a0034db4dd46e4
Jiratta Areekhan <6610110046@email.psu.ac.th>
2024-01-29 22:05:00 +0700

    add 10 page

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
-from flask import Flask
+from flask import Flask ,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, Flask!"
+@app.route('/')
+def home():    
+    return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template('articles.html')
+
+@app.route('/about')
+def about():
+    return render_template('brands.html')
+
+@app.route('/about')
+def about():
+    return render_template('contact.html')
+
+@app.route('/about')
+def about():
+    return render_template('faq.html')
+
+@app.route('/about')
+def about():
+    return render_template('idoluse.html')
+
+@app.route('/about')
+def about():
+    return render_template('login.html')
+
+@app.route('/about')
+def about():
+    return render_template('perfumes.html')
+
+@app.route('/about')
+def about():
+    return render_template('regiter.html')
+
+if __name__ =="__main__":
+    app.run(debug=True)
