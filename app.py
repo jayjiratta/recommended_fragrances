e8029da6022d6c5981c771897e5f2c63a145ba98
Jiratta Areekhan <6610110046@email.psu.ac.th>
2024-02-03 01:33:45 +0700

    debug some error

--- app.py
+++ app.py
@@ -1,8 +1,55 @@
-from flask import Flask ,render_template
+from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 
-@app.route('/Home')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('blog'))
+        else:
+            flash('Invalid username or password', 'error')  
+            return redirect(url_for('login'))
+    flash_messages = list(get_flashed_messages())
+    return render_template('Login_card.html', flashed_messages=flash_messages)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get('username')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if not username:
+            flash('Username is required', 'error')
+            return redirect(url_for('register'))
+
+        user_exists = User.query.filter_by(username=username).first()
+        if user_exists:
+            return 'Username already exists'
+
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, name=name, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/')
 def home():    
     return render_template("home.html")
 
